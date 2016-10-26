1605d4ff3adab0b0c15e95b86aff7bdf2268b3f6
Youssef Gaber <ygabrola@gmail.com>
2016-10-26 03:10:57 +0200

    Add timeout

--- src/Connection/RestClient.php
+++ src/Connection/RestClient.php
@@ -76,7 +76,7 @@ class RestClient {
                     'User-Agent' => Blocktrail::SDK_USER_AGENT . '/' . Blocktrail::SDK_VERSION
                 ),
                 'exceptions' => false,
-                'connect_timeout' => 3,
+                'connect_timeout' => 10,
                 'timeout' => 300.0, // tmp until we have a good matrix of all the requests and their expect min/max time
                 'verify' => true,
                 'proxy' => '',
@@ -120,8 +120,8 @@ class RestClient {
                 'User-Agent' => Blocktrail::SDK_USER_AGENT . '/' . Blocktrail::SDK_VERSION
             ),
             'http_errors' => false,
-            'connect_timeout' => 3,
-            'timeout' => 20.0, // tmp until we have a good matrix of all the requests and their expect min/max time
+            'connect_timeout' => 10,
+            'timeout' => 300.0, // tmp until we have a good matrix of all the requests and their expect min/max time
             'verify' => true,
             'proxy' => '',
             'debug' => false,
